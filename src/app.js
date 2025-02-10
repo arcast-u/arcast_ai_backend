6e06faf6cb90358cadaf01c0327a2881c4df3f78
Habeeb Dindi <habeeb@alphanexttech.com>
2025-02-10 23:36:52 +0200

    Add vercel config

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ import { specs } from './docs/swagger.js';
 import studioRoutes from './routes/studio.routes.js';
 import bookingRoutes from './routes/booking.routes.js';
 
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
+
 dotenv.config();
 
 const app = express();
@@ -15,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 // Swagger documentation route
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs, { customCssUrl: CSS_URL }));
 
 app.use('/api/studios', studioRoutes);
 app.use('/api/bookings', bookingRoutes);
